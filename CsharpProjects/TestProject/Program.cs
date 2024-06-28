607b505e38095a2633b62180404958691344766f
LRondinelli04 <lucasrondinelli04@gmail.com>
2024-06-28 09:37:34 -0300

    Modulo 2 - C#

--- CsharpProjects/TestProject/Program.cs
+++ CsharpProjects/TestProject/Program.cs
@@ -1,8 +1,10 @@
-﻿Random dice = new Random();
-int roll1 = dice.Next();
-int roll2 = dice.Next(101);
-int roll3 = dice.Next(50, 101);
+﻿// Pruebas de operaciones aritméticas y de asignación
 
-Console.WriteLine($"First roll: {roll1}");
-Console.WriteLine($"Second roll: {roll2}");
-Console.WriteLine($"Third roll: {roll3}");
+int a = 5;
+int b = 10;
+
+Console.WriteLine(a + b);
+
+Console.WriteLine($"Resta: {a} - {b} = {a - b}");
+
+Console.WriteLine(5 / 10);
--- CsharpProjects/TestProject/obj/Debug/net8.0/TestProject.AssemblyInfo.cs
+++ CsharpProjects/TestProject/obj/Debug/net8.0/TestProject.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("TestProject")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+789ed535d3f79375a976ba8b3b574a4ba149dbe7")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+f4f92694f35ded118947e1e84f4782949427d372")]
 [assembly: System.Reflection.AssemblyProductAttribute("TestProject")]
 [assembly: System.Reflection.AssemblyTitleAttribute("TestProject")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
